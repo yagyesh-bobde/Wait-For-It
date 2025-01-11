a9d82b64ce91769ddd2730c1e4ab8e2197c66dff
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2025-01-11 05:54:44 +0000

    feat: remove strick mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import Checker from "vite-plugin-checker";
 
 export default defineConfig({
   plugins: [
     react(),
-    Checker({
-      typescript: false, // Disable TypeScript type checking
-    }),
   ],
   resolve: {
     alias: {
